9e0f03139612c25647fc72c155e98fb6345b99ca
David Tsai <>
2023-01-30 18:05:39 +0800

    add practice code

--- Javascript/Practice/Event/event.js
+++ Javascript/Practice/Event/event.js
@@ -4,6 +4,8 @@ const title = document.querySelector("h1");
 const addBtn = document.querySelector(".add-btn");
 const minusBtn = document.querySelector(".minus-btn");
 const result = document.querySelector(".result");
+const cityList = document.querySelector(".cityList");
+
 
 // 嘗試綁定"監聽"事件
 btn.addEventListener("click", function(e) {
@@ -16,9 +18,11 @@ let count = 0
 addBtn.addEventListener("click", function(e) {
    count += 1;
    result.innerHTML = count;
-   // event　物件 -> 告訴你當下的元素資訊
+   // event　物件 -> 告訴你當下點擊的元素資訊
    console.log(e);
-   console.log(e.clientX);
+   console.log("加法器參數" + e);
+   console.log(e.target);
+   console.log(e.target.nodeName);
 })
 
 // 減法器JS
@@ -29,7 +33,29 @@ minusBtn.addEventListener("click", function(e) {
       alert("Count 不能為負數！")
    }
    result.innerHTML = count; 
+   console.log(e);
+   console.log("減法器參數" + e);
+   console.log(e.target);
+   console.log(e.target.nodeName);
 })
+
+
+//DOM e.target 取值練習
+const firstBtn = document.querySelector(".first-btn");
+const secondBtn = document.querySelector(".second-btn");
+const thirdBtn = document.querySelector(".third-btn");
+const targetResult = document.querySelector(".targetResult");
+
+firstBtn.addEventListener("click", function (e) {
+  targetResult.innerHTML = `<h1>測試結果1</h1>`;
+  console.log(e);
+});
+secondBtn.addEventListener("click", function (e) {
+  targetResult.innerHTML = `<h1>測試結果2</h1>`;
+});
+thirdBtn.addEventListener("click", function (e) {
+  targetResult.innerHTML = `<h1>測試結果3</h1>`;
+});
 
 
 
@@ -44,3 +70,4 @@ minusBtn.addEventListener("click", function(e) {
 //    districtList.value = "請選擇區域";
 // }
 // 試著用 JS 直接實作地址輸入器 END
+
